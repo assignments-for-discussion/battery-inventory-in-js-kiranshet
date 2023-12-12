97b0a953689701d860f80efe38ab43679e5281e7
kiran shet <54165702+kiranshet@users.noreply.github.com>
2023-12-12 12:09:39 +0530

    Update main.js
    task

--- main.js
+++ main.js
@@ -1,21 +1,44 @@
 const assert = require('assert');
 
-function countBatteriesByHealth(presentCapacities) {
-  return {
-    healthy: 0,
-    exchange: 0,
-    failed: 0
-  };
-}
+// function countBatteriesByHealth(presentCapacities) {
+//   return {
+//     healthy: 0,
+//     exchange: 0,
+//     failed: 0
+//   };
+// }
 
 function testBucketingByHealth() {
   console.log('Counting batteries by SoH...');
   const presentCapacities = [113, 116, 80, 95, 92, 70];
-  counts = countBatteriesByHealth(presentCapacities);
-  assert(counts["healthy"] == 2);
-  assert(counts["exchange"] == 3);
-  assert(counts["failed"] == 1);
-  console.log("Done counting :)");
+  
+  // counts = countBatteriesByHealth(presentCapacities);
+  // assert(counts["healthy"] == 2);
+  // assert(counts["exchange"] == 3);
+  // assert(counts["failed"] == 1);
+  // console.log("Done counting :)");
+  let healthy = [];
+  let exchange = [];
+  let failed = [];
+  for(let i=0; i<presentCapacities.length; i++)
+    {
+        if(presentCapacities[i]>=80 && presentCapacities[i]<=100)
+        {
+          healthy.push(presentCapacities[i]);
+         }
+        else if(presentCapacities[i]>=62 && presentCapacities[i]<80)
+        {
+          exchange.push(presentCapacities[i])
+        }
+       else 
+        {
+          failed.push(presentCapacities[i])
+        }
+    }
+
+  let healthyCount = healthy.length;
+  let exchangeCount = exchange.length;
+  let failedCount = failed.length;
 }
 
 testBucketingByHealth();
